101df86e30c44dd8d3867ef45eb9c6538f9fcebb
Trevor H <h.trevs@outlook.com>
2023-03-30 13:33:36 -0500

    Add methods to place mark inside empty tile and change player turns

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 /**
- * Game Class Object handles DOM events, maintain state
+ * Game Class handles DOM events, maintain state
  * of game, and implement required methods for rules
  */
 class Game {
     constructor() {
+        this.playerTwoTurn = false;
         this.tileEls = document.querySelectorAll(".tile");
 
-        this.tileEls.forEach(tile => tile.addEventListener("click", this.handleClick));
+        // Invoke only once for each empty tile
+        this.tileEls.forEach(tile => tile.addEventListener("click", this.handleClick, {once: true}));
     }
 
+    /**
+     * Check win conditions and player turns after 
+     * clicking an empty tile and placing a token
+     * @param {"click"} event 
+     */
     handleClick = event => {
-        console.log("Clicked!");
+        const emptyTile = event.target;
+        const currMark = this.playerTwoTurn ? "O" : "X";
+        this.placeMark(currMark, emptyTile);
+        this.switchPlayersTurn();
+        console.log(emptyTile);
+    }
+
+    placeMark = (mark, tile) => {
+        tile.innerText = mark;
+    }
+
+    switchPlayersTurn = () => {
+        this.playerTwoTurn = !this.playerTwoTurn;
     }
 }
 
